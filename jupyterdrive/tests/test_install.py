977e12cf55485435c9851f2d5fea459407dff871
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-04-22 14:21:21 -0700

    implement deactivate

--- jupyterdrive/tests/test_install.py
+++ jupyterdrive/tests/test_install.py
@@ -1,10 +1,23 @@
 from __future__ import print_function, absolute_import
 
 
-from jupyterdrive import install
+from jupyterdrive import deactivate,install, activate
 
 
 
 def test_install():
     install(verbose=True, mixed=True, user=True)
+
+def test_deactivate():
+    deactivate()
+
+def test_deactivate_2():
+    deactivate()
+
+def test_install_normal():
+    install(verbose=True, mixed=False, user=True)
+
+def test_deactivate_2():
+    deactivate()
+
 
